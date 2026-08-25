5bc7aeed99ea3f6c3f53e77f1a7624637759693c
Samo MacBook Air <samo.remec@gmail.com>
2026-08-25 19:54:39 +0200

    Added About, Credits, Licenses screens

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 // Bump to discard every previously cached response
-const CACHE_NAME = 'na-tuner-v14';
+const CACHE_NAME = 'na-tuner-v15';
 
 // Everything the app needs to open with no network at all
 const SHELL = [
   './',
   './index.html',
   './tuner.html',
+  './about.html',
+  './licences.html',
   './style.css',
   './ozadje.jpg',
   './tuner.js',
